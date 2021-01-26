a7f2aa186fb95dc2a4b2c6a07e1f67046b2b67c3
Asashou <aisha.hoshi@gmail.com>
2021-01-26 13:05:04 +0100

    function for converting roman num into arabic num

--- roman_to_num/roman_to_num.py
+++ roman_to_num/roman_to_num.py
@@ -2,17 +2,18 @@ def roman_to_num(roman: str):
     look_up = {'I':1, 'V':5, "X":10, 'L':50, 'C':100, 'D':500, 'M':1000}
     num = 0
     for ind, l in enumerate(roman):
-        value = look_up[l]
         try:
-            next = roman[ind + 1]
-            if value < look_up[next]:
-                print('num', num, 'next', ind+1, look_up[next])
-                value = -1 * value
-                print('value', value)
-        except:
-            break
-        num += value
-        print(num)
+            value = look_up[l]
+            try:
+                next = roman[ind + 1]
+                if value < look_up[next]:
+                    value = -1 * value
+            except:
+                value = value
 
-    return value
+            num += value
+        except:
+            raise TypeError('The provided Roman number cannot be recognized')
+
+    return num
 
